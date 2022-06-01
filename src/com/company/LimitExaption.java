89a3af2c70cb1713ef5817ff6e18a4a5678baf97
muhammadyusuf <abdurasulovmuhammadyusuf235@gmail.com>
2022-06-01 15:19:12 +0600

    Initial commit

---
+++ src/com/company/LimitExaption.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class LimitExaption extends Exception{
+   private double remainin;
+
+    public LimitExaption(String message, double remainin) {
+        super(message);
+        this.remainin = remainin;
+    }
+
+    public double getRemainin() {
+        return remainin;
+    }
+
+    protected void GetRemaininAmout(){
+
+  }
+}
